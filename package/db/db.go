85badfa6d82fe076274edc83029499c12b9487d6
aarock1234 <abdelseaf@gmail.com>
2026-03-01 03:40:57 -0800

    fix generate

--- package/db/db.go
+++ package/db/db.go
@@ -1,7 +1,7 @@
 // Package db wraps pgxpool with sqlc-generated queries, transaction helpers,
 // and advisory lock support.
 //
-//go:generate sqlc generate -f package/db/sqlc.yml
+//go:generate sqlc generate -f sqlc.yaml
 package db
 
 import (
